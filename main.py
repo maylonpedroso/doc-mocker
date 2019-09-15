972ee4b1eb52dc23c695a3769122bcaefd8f7645
Maylon Pedroso <maylonpedroso@gmail.com>
2019-09-15 17:37:56 -0500

    Improve plugins handling - validate plugins definition - process plugins by the CLI args order

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
-
+import logging
 import sys
+
+from typing import List
 
 from doc_mocker.manager import Manager
 from doc_mocker.models import PageType
@@ -8,7 +10,10 @@ from doc_mocker.seeders import PageSeeder
 from doc_mocker.plugin_loader import PluginLoader
 
 
-def run():
+logger = logging.getLogger(__name__)
+
+
+def run() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("command", help="command to run", choices=["generate"])
     parser.add_argument(
@@ -36,26 +41,47 @@ def run():
         "-o", "--output-path", help="output path (default: current path)", default="."
     )
 
+    if '-h' not in sys.argv and '--help' not in sys.argv:
+        main_args, argv = parser.parse_known_args()
+
     # setting up plugin arguments
-    plugins = PluginLoader.get_all()
-    for name, plugin in plugins.items():
+    plugins_index = {}
+    for name, plugin in PluginLoader.get_all().items():
         for arg_name, arg_config in plugin.arguments:
             parser.add_argument(*arg_name, **arg_config)
+            plugins_index[arg_name[0]] = plugin
 
-    args = parser.parse_args()
+    parser.parse_args()
 
-    try:
-        plugins = [plugin.cls(args) for plugin in plugins.values()]
-    except ValueError as e:
-        sys.exit(e)
+    plugins = []
+    for argv in split_plugins_arguments(argv):
+        plugin = plugins_index[argv[0]]
+        parser = argparse.ArgumentParser()
+        for arg_name, arg_config in plugin.arguments:
+            parser.add_argument(*arg_name, **arg_config)
+        namespace, _ = parser.parse_known_args(argv)
+        try:
+            plugins.append(plugin.cls(namespace))
+        except Exception as e:
+            logger.error(f"Plugin setup failed [{' '.join(argv)}]: {e}")
 
     Manager(
-        pages=args.number_of_pages,
-        type_=args.page_type,
-        seeder=args.page_seeder,
-        output_path=args.output_path,
+        pages=main_args.number_of_pages,
+        type_=main_args.page_type,
+        seeder=main_args.page_seeder,
+        output_path=main_args.output_path,
         plugins=plugins,
-    ).run(args.command)
+    ).run(main_args.command)
+
+
+def split_plugins_arguments(arguments: List[str]) -> List[List[str]]:
+    split_args = []
+    for argument in arguments:
+        if argument.startswith('-'):
+            split_args.append([argument])
+        else:
+            split_args[-1].append(argument)
+    return split_args
 
 
 if __name__ == "__main__":
